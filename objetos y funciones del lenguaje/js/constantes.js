4f72f703ed96bed683f025860fe6a401c8f7ccc7
unknown <roland8410@hotmail.com>
2023-01-18 13:12:44 -0600

    version 1.0.1 miercoles 18_enero

---
+++ objetos y funciones del lenguaje/js/constantes.js
@@ -0,0 +1,10 @@
+export const PI= Math.PI;
+
+export let usuario= "Hugo";
+let password="Qwety";
+export default password;
+
+const hello = ()=> console.log('hola');
+export function saludar() {
+    console.log('Hola modulos ES6')
+}
--- objetos y funciones del lenguaje/js/modulosImporExport.js
+++ objetos y funciones del lenguaje/js/modulosImporExport.js
@@ -1,1 +1,12 @@
-console.log('hola')
+import  password, {PI,usuario} from "./constantes.js";
+import {aritmetica} from "./aritmetica.js";
+
+
+console.log('Archivo modulos')
+
+console.log(PI,usuario)
+console.log(password);
+
+console.log(aritmetica.suma(3,4))
+console.log(aritmetica.resta(10,4))
+
